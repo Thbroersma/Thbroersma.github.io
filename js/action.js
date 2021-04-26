9ad329b8da39499c9be446bb5a2c9155d2ba0616
Thierry Broersma <30267794@student.rocmondriaan.nl>
2021-04-26 17:28:11 +0200

    Javascript moet je adden natuurlijk en een start gemaakt voor de thema switcher

---
+++ js/action.js
@@ -0,0 +1,34 @@
+const gameButton = document.querySelector('.gameButton');
+let gameQuest = document.querySelector('.gamePlay');
+const checkButton = document.querySelector('.gameCheck');
+const inputAnswer = document.querySelector('.answers');
+const switchButton = document.querySelector('.switchButton');
+
+let gameQuestions = [ 
+    'What do you must come in Javascript to make a button do something?', 
+    //'What is the longest distance that the web-developer had'
+];
+
+if (gameButton) {
+    gameButton.addEventListener('click', function () {
+        gameQuest.innerHTML = gameQuestions;
+        //gameQuest.innerHTML = gameQuestions[Math.floor(Math.gameQuest() * gameQuestions.length)];
+        checkButton.classList.toggle('gameButton');
+        checkButton.innerHTML = 'Check your answer'
+    })
+
+}
+if (checkButton) {
+    checkButton.addEventListener('click', function() {
+        if (gameQuest.innerHTML.includes('button')) {
+            if (inputAnswer.value.includes('addEventListener')) {
+                alert('You got it right!');
+            }
+            else {
+                alert("No you didn't got it right, sorry try again");
+            }
+            console.log(inputAnswer.innerHTML);
+        }
+        
+    })
+}
