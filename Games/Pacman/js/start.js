92bc67c1f8ef7c8494bc5243d069bf817b31627c
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2022-03-21 20:53:03 +0100

    Veel dingen aangepast voor een professioneel portfolio

--- Games/Pacman/js/start.js
+++ Games/Pacman/js/start.js
@@ -1,5 +1,6 @@
 const singlePlayer = document.querySelector('.single');
 const multiPlayer = document.querySelector('.multi');
+const startPlay = document.querySelector('.start');
 
 if (singlePlayer) {
     singlePlayer.addEventListener('click', function() {
@@ -9,5 +10,10 @@ if (singlePlayer) {
 if (multiPlayer) {
     multiPlayer.addEventListener('click', function() {
         location.replace('players2.html');
+    })
+}
+if (startPlay) {
+    startPlay.addEventListener('click', function() {
+        location.replace('Pacman/index.html');
     })
 }
