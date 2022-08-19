7abd55d24ad39be3a32247ae3ecbd07c74c24d2e
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2022-08-19 15:44:59 +0200

    De editor werkt geheel met html, css en javascript

--- js/quiz.js
+++ js/quiz.js
@@ -79,13 +79,17 @@ if (scoreButton) {
 // the website editor 
 function run() {
     let htmlCode = document.querySelector(".editor #html-code").value;
-    let cssCode = document.querySelector(".editor #css-code").value;
+    let cssCode = "<style>" + document.querySelector(".editor #css-code").value+"</style>";
     let jsCode = document.querySelector(".editor #js-code").value;
     let output = document.querySelector(".editor #output ");
     //console.log(htmlCode, cssCode, jsCode, output);
 
-    output.contentDocument.body.innerHTML = htmlCode;
+    output.contentDocument.body.innerHTML = htmlCode+cssCode;
+    output.contentWindow.eval(jsCode);
 }
+document.querySelector(".editor #html-code").addEventListener("keyup", run);
+document.querySelector(".editor #css-code").addEventListener("keyup", run);
+document.querySelector(".editor #js-code").addEventListener("keyup", run);
 
 // Function for checking the answer correctly
 if (checkButton) {
