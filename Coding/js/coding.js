c16654fd93f09a4fd4d7d7d4444c26baa220448d
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2023-04-20 12:09:55 +0200

    Layout verbeteringen

--- Coding/js/coding.js
+++ Coding/js/coding.js
@@ -1,13 +1,10 @@
 const runcode = document.querySelector('.runCode');
-
-
+const Thebody = document.querySelector('.myBody');
 function run() {
     let htmlCode = document.querySelector(".editor #html-code").value;
     let cssCode = "<style>" + document.querySelector(".editor #css-code").value+"</style>";
     let jsCode = document.querySelector(".editor #js-code").value;
     let output = document.querySelector(".editor #output");
-
-
 
     output.contentDocument.body.innerHTML = htmlCode+cssCode;
     output.contentWindow.eval(jsCode);
@@ -17,4 +14,7 @@ if (runcode) {
     runcode.addEventListener('click', function() {
         run();
     })
+}
+if (Thebody.contains("Let the games begin")) {
+    alert("You've got it")
 }
