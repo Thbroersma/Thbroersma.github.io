cbcce21dabb8cce686b3292e05f59aa853d8d55f
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2023-04-27 17:33:06 +0200

    Eerste opzet voor uitdaging web development. Plus wat uitlijning rond margin/padding recht gezet

--- Coding/js/coding.js
+++ Coding/js/coding.js
@@ -1,5 +1,9 @@
 const runcode = document.querySelector('.runCode');
 const Thebody = document.querySelector('.myBody');
+const startFade = document.querySelector('.first');
+const middleFade = document.querySelector('.last');
+const firstview = document.querySelector('.first-view');
+const secondview = document.querySelector('.second-view');
 function run() {
     let htmlCode = document.querySelector(".editor #html-code").value;
     let cssCode = "<style>" + document.querySelector(".editor #css-code").value+"</style>";
@@ -9,12 +13,19 @@ function run() {
     output.contentDocument.body.innerHTML = htmlCode+cssCode;
     output.contentWindow.eval(jsCode);
 }
-
+function sleep () {
+    setTimeout(addClass,4000);
+}
+function addClass() {
+    startFade.style.display = "none";
+    firstview.style.display = "none";
+    secondview.style.display = "block";
+    middleFade.style.display = "block";
+}
+sleep();
 if (runcode) {
     runcode.addEventListener('click', function() {
         run();
     })
 }
-if (Thebody.contains("Let the games begin")) {
-    alert("You've got it")
-}
+
