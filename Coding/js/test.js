3d4ad381d8279d6a2076e7382ee41449f9a22899
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2023-07-11 10:02:21 +0200

    Opdrachten voor titels toegevoegd

---
+++ Coding/js/test.js
@@ -0,0 +1,40 @@
+const opdrachtEen = document.querySelector('.answerOne');
+
+const opdrachtTwo = document.querySelector('.opTwo');
+const opdrachtThree = document.querySelector('.answerThree');
+
+const opdrachtVier = document.querySelector('.opFour');
+const checkEen= document.querySelector('.checkTwo');
+if (checkEen) {
+    checkEen.addEventListener('click', function () {
+        if(opdrachtEen.checked) {
+            alert("Je hebt vraag 1 goed!")
+        }
+        else {
+            alert("Je hebt vraag 1 nog niet goed, probeer het opnieuw")
+        }
+        if(opdrachtTwo.value === "h2" || opdrachtTwo.value === "H2") {
+            alert("Je hebt vraag 2 goed!")
+        }
+        else {
+            alert("Je hebt vraag 2 nog niet goed, probeer het opnieuw")
+        }
+        if(opdrachtThree.checked) {
+            alert("Je hebt vraag 3 goed!")
+        }
+        else {
+            alert("Je hebt vraag 3 nog niet goed, probeer het opnieuw")
+        }
+        if(opdrachtVier.value === "<h1>Laika</h1>" || opdrachtVier.value === "<H1>Laika</H1>") {
+            alert("Je hebt de goeie tag in vraag 4!")
+        }
+        else {
+            alert("Je hebt vraag 4 nog niet goed, probeer het opnieuw")
+        }
+    })
+}
+
+/*
+if(checkEen) {
+    alert("button")
+}*/
