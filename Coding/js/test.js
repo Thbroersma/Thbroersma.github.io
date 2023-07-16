f4db98303507d357feb2314fcc9de9aa2f412a62
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2023-07-16 15:22:07 +0200

    Dingen aan het aanvullen en verbeteren

--- Coding/js/test.js
+++ Coding/js/test.js
@@ -4,6 +4,12 @@ const opdrachtThree = document.querySelector('.answerThree');
 const actionButton = document.querySelector('.activeCode')
 const opdrachtVier = document.querySelector('.opFour');
 const checkEen= document.querySelector('.checkTwo');
+const pOne = document.querySelector('.pOne');
+const pTwo = document.querySelector('.pTwo');
+const pThree = document.querySelector('.pThree');
+const pTree = document.querySelector('.pTree');
+const checkTwo= document.querySelector('.checkP');
+
 if (checkEen) {
     checkEen.addEventListener('click', function () {
         if(opdrachtEen.checked) {
@@ -30,6 +36,35 @@ if (checkEen) {
         else {
             alert("Je hebt vraag 4 nog niet goed, probeer het opnieuw")
         }
+    })
+}
+if (checkTwo) {
+    checkTwo.addEventListener('click', function () {
+        if(pOne.checked) {
+            alert("Je hebt vraag 1 goed!")
+        }
+        else {
+            alert("Je hebt vraag 1 nog niet goed, probeer het opnieuw")
+        }
+        if(pTwo.value === "</p>" || pTwo.value === "</P>") {
+            alert("Je hebt vraag 2 goed!")
+        }
+        else {
+            alert("Je hebt vraag 2 nog niet goed, probeer het opnieuw")
+        }
+        if((pThree === "<h1></h1>" || pThree === "<H1></H1>")) {
+            alert("Je hebt vraag 3 goed!")
+        }
+        if (pThree === "<h1></h1>" || pThree === "<H1></H1>"){
+            alert("Je hebt vraag 3, regel 5 goed! Regel 4 moet je nog verbeteren")
+        }
+        else if (pTree === "<p></p>" || pTree === "<P></P>"){
+            alert("Je hebt vraag 3, regel 4 goed! Regel 5 moet je nog verbeteren")
+        }
+        else {
+            alert("Je hebt vraag 3 nog niet goed, probeer het opnieuw")
+        }
+
     })
 }
 function run() {
