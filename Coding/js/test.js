57740f9eda9db09e4b46fb6cb5b3ede5c9ef100f
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2023-07-19 16:38:16 +0200

    Opdrachten voor image afgemaakt

--- Coding/js/test.js
+++ Coding/js/test.js
@@ -9,7 +9,12 @@ const pTwo = document.querySelector('.pTwo');
 const pThree = document.querySelector('.pThree');
 const pTree = document.querySelector('.pTree');
 const checkTwo= document.querySelector('.checkP');
-
+const IOne = document.querySelector('.IOne');
+const ITwo = document.querySelector('.ITwo');
+const IThree = document.querySelector('.IThree');
+const ITree = document.querySelector('.ITree');
+const IThee = document.querySelector('.IThee');
+const Icheck= document.querySelector('.Icheck');
 if (checkEen) {
     checkEen.addEventListener('click', function () {
         if(opdrachtEen.checked) {
@@ -52,12 +57,41 @@ if (checkTwo) {
         else {
             alert("Je hebt vraag 2 nog niet goed, probeer het opnieuw")
         }
-        if((pThree === "<h1></h1>" || pThree === "<H1></H1>")) {
+        if((pThree.value === "<h1></h1>" || pThree.value === "<H1></H1>") && (pTree.value === "<p></p>" || pTree.value === "<P></P>")) {
             alert("Je hebt vraag 3 goed!")
         }
-        if (pTree === "<p></p>" || pTree === "<P></P>"){
+        else if (pTree.value === "<p></p>" || pTree.value === "<P></P>"){
             alert("Je hebt vraag 3, regel 5 goed! Regel 4 moet je nog verbeteren")
         }
+        else if (pThree.value === "<h1></h1>" || pThree.value === "<H1></H1>") {
+            alert("Je hebt vraag 3, regel 4 goed. Verbeter regel 5")
+        }
+        else {
+            alert("Je hebt vraag 3 nog niet goed, probeer het opnieuw")
+        }
+
+    })
+}
+if (Icheck) {
+    Icheck.addEventListener('click', function () {
+        if(IOne.checked) {
+            alert("Je hebt vraag 1 goed!")
+        }
+        else {
+            alert("Je hebt vraag 1 nog niet goed, probeer het opnieuw")
+        }
+        if(ITwo.value === "src='hond.png'" || ITwo.value === 'src="hond.png"') {
+            alert("Je hebt vraag 2 goed!")
+        }
+        else {
+            alert("Je hebt vraag 2 nog niet goed, probeer het opnieuw")
+        }
+        if((IThree.value === "<h1></h1>" || IThree.value === "<H1></H1>") && (ITree.value === "<p></p>" || ITree.value === "<P></P>") && (IThee.value === "<img>" || IThee.value === "<IMG>" || IThee.value === "<img src='harry.jpg'>")) {
+            alert("Je hebt vraag 3 goed!")
+        } 
+        else if((IThree.value === "<h1></h1>" || IThree.value === "<H1></H1>") || (ITree.value === "<p></p>" || ITree.value === "<P></P>") || (IThee.value === "<img>" || IThee.value === "<IMG>" || IThee.value === "<img src='harry.jpg'>")) {
+            alert("Er zitten nog een of meerdere fouten in je code. Maar je bent op de goede weg!")
+        } 
         else {
             alert("Je hebt vraag 3 nog niet goed, probeer het opnieuw")
         }
