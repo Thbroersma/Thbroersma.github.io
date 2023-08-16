9f60e484d72f69f5e21e5002f8161602b8ba284a
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2023-08-16 13:37:39 +0200

    Op de HTML totaal test bezig met op een creatievere manier checken of de code is ingevuld

--- Coding/HTML/tests/html.js
+++ Coding/HTML/tests/html.js
@@ -1,3 +1,6 @@
+const check = document.querySelector(".check");
+let htmlCode = document.querySelector("#html-code");
+const nextCSS = document.querySelector(".nextCSS")
 const runningcode = document.querySelector('.runningCode');
 function run() {
     let htmlCode = document.querySelector(".editor #html-code").value;
@@ -5,6 +8,25 @@ function run() {
     let style = "<style> img { width:60vw; } </style>"
     output.contentDocument.body.innerHTML = htmlCode + style;
 }
+if(htmlCode) {
+    htmlCode.addEventListener("click", function() {
+        if (htmlCode.value.includes("<h1></h1>")) {
+            nextCSS.href = "../../CSS-C/cssIntro.html";
+            check.classList.remove("next");
+        } else {
+            check.classList.add("next");
+        }                           
+    })
+}
+
+/*
+if (check) {
+    check.addEventListener("click", function() {
+       
+    })
+}*/
+
+
 if (runningcode) {
     runningcode.addEventListener('click', function() {
         run();
