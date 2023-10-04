9db1d6fbaf27ebadc0c5de88f945c891ca9f72a2
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2023-10-04 15:11:45 +0200

    Foto's van mezelf opgedraaid, dat de professionale eerst komt. Bezig met het aanpassen van de begin pagina van de cursus. Door er een voorbeeld van pacman op te zetten

--- Coding/js/coding.js
+++ Coding/js/coding.js
@@ -5,6 +5,35 @@ const middleFade = document.querySelector('.last');
 const firstview = document.querySelector('.first-view');
 const learningview = document.querySelector('.learning-view');
 const secondview = document.querySelector('.second-view');
+// pacman view
+const grid = document.querySelector('.grid');
+const layout = [
+    0, 1, 0, 0, 0, 1,
+    0, 0, 0, 1, 0, 0,
+    1, 1, 1, 0, 2, 0,
+    0, 0, 1, 0, 0, 0,
+    1, 0, 0, 0, 1, 0,
+    0, 0, 0, 0, 0, 0,
+  ];
+const squares = [];
+function createBoard() {
+  
+    for (let i = 0; i < layout.length; i++) {
+      const square = document.createElement('div');
+      // Here is the grid filed with div how get after there own styling
+      grid.appendChild(square);
+      squares.push(square);
+      if (layout[i] === 0) {
+        squares[i].classList.add('field'/*, 'pac-dot'*/);
+      } else if (layout[i] === 1) {
+        squares[i].classList.add('field-box');
+      } else if (layout[i] === 2) {
+        squares[i].classList.add('pac-man');
+      }
+    }
+  }
+  createBoard();
+// block to make the users code on the website work
 function run() {
     let htmlCode = document.querySelector(".editor #html-code").value;
     let cssCode = "<style>" + document.querySelector(".editor #css-code").value+"</style>";
@@ -14,9 +43,11 @@ function run() {
     output.contentDocument.body.innerHTML = htmlCode+cssCode;
     output.contentWindow.eval(jsCode);
 }
+//
 function sleep () {
     setTimeout(addClass,4000);
 }
+// Change the images for the changed website
 function addClass() {
     startFade.style.display = "none";
     firstview.style.display = "none";
