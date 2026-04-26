c37fe34e5936a7ecbc166577ae219939f9d5f584
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2026-04-26 13:06:58 +0200

    Data geojson points toegevoegd

---
+++ Games/personal_project/js/pokemons/general/generalNormal.js
@@ -0,0 +1,10 @@
+var generalOne1 = {"type":"Feature","properties":{},"geometry":{"coordinates":[5.3965599090222724,52.15059002308294],"type":"Point"}}
+
+var generalOne2 = {"type":"Feature","properties":{},"geometry":{"coordinates":[5.399180775237767,52.15095369824161],"type":"Point"}}
+var generalOne3 = {"type":"Feature","properties":{},"geometry":{"coordinates":[5.399662347829661,52.14927167572992],"type":"Point"}}
+var generalTwo1 = {"type":"Feature","properties":{},"geometry":{"coordinates":[5.401314474696761,52.14716051311427],"type":"Point"}}
+var generalTwo2 = {"type":"Feature","properties":{},"geometry":{"coordinates":[5.4011757082628264,52.14415170715105],"type":"Point"}}
+var generalTwo3 = {"type":"Feature","properties":{},"geometry":{"coordinates":[5.404506102674645,52.14423686483235],"type":"Point"}}
+
+export var listOne = [generalOne1, generalOne2, generalOne3]
+export var listTwo = [generalTwo1, generalTwo2, generalTwo3]
---
+++ Games/personal_project/js/pokemons/water/generalPoints.js
@@ -0,0 +1,68 @@
+export var waterGeneral1 = {
+    "type":"Feature",
+    "properties":{
+
+    },
+    "geometry": {
+        "coordinates":[5.406849359844188,52.13508558605912],
+        "type":"Point"
+        }
+    }
+export var waterGeneral2 = {
+    "type":"Feature",
+    "properties":{
+
+    },
+    "geometry":{
+        "coordinates":[5.400289568641142,52.14660540983431],
+        "type":"Point"
+        }
+    }
+export var waterGeneral3 = {
+  "type":"Feature",
+  "properties":{
+
+  },
+  "geometry":{
+    "coordinates":[5.391076759174268,52.13540913403742],
+    "type":"Point"}
+  }
+export var waterGeneral4 = {
+  "type":"Feature",
+  "properties":{
+
+  },
+  "geometry":{
+    "coordinates":[5.4168176154771,52.13240440468584],
+    "type":"Point"}}
+export var waterGeneral5 = {
+  "type":"Feature",
+  "properties":{
+  },"geometry":{
+    "coordinates":[5.405700167975965,52.17171031241216],
+    "type":"Point"}}
+export var waterGeneral6 = {
+  "type":"Feature",
+  "properties":{},
+  "geometry":
+  {"coordinates":[5.3841821218836685,52.17844097007071],
+    "type":"Point"}}
+export var waterGeneral7 = {
+  "type":"Feature",
+  "properties":{},
+  "geometry":{
+    "coordinates":[5.395314005475825,52.116579991571825],
+    "type":"Point"}}
+export var startMap = 
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          5.38967691458771,
+          52.15626388584985
+        ],
+        "type": "Point"
+      }
+    }
+
