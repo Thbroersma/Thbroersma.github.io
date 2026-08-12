25a415ba1c5a41d4bcfd382dd789f5c672aec7ee
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2026-08-12 16:24:11 +0200

    Test class toevoegen andere manier

--- find/babaMap.js
+++ find/babaMap.js
@@ -70,11 +70,13 @@ var imageUrlRight = "footstepRight.png";
 
 var coordinates = L.latLngBounds([52.14233006663446, 5.399290711598951], [52.14236883812015, 5.399411445488624]);
 var coordinatesRight = L.latLngBounds([52.142311, 5.39929], [52.142264, 5.399413]);
-var imageOverlay2 = L.imageOverlay(imageUrlRight, coordinatesRight);
-var imageOverlay = L.imageOverlay(imageUrl, coordinates);
+var imageOverlay2 = L.imageOverlay([imageUrlRight, coordinatesRight], {
+  className: "right-foot",
+});
+var imageOverlay = L.imageOverlay([imageUrl, coordinates], {
+  className: "left-foot",
+});
 
-imageOverlay.getElement().classList.add('left-foot');
-imageOverlay2.getElement().classList.add('right-foot');
 
 imageOverlay.addTo(map);
 imageOverlay2.addTo(map);
