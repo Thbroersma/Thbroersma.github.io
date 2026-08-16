c8df7680547f3cfba6abe99f7beb13509b771599
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2026-08-16 14:12:42 +0200

    Correctie

--- find/babaMap.js
+++ find/babaMap.js
@@ -111,13 +111,14 @@ let loop = 1;
 let rightFoot = document.querySelector(".right-foot");
 let leftFoot = document.querySelector(".left-foot");
 
-const myTimeout = setTimeout(stopMoving(), 30000);
 rightFoot.id = "right-foot";
 leftFoot.id = "left-foot";
 let right = document.getElementById("right-foot");
 let left = document.getElementById("left-foot");
 let movingRight = setInterval(move(), 3000);
 let movingLeft = setInterval(move2(), 3000);
+const myTimeout = setTimeout(stopMoving(), 30000);
+
 function move() {
   right.style.marginLeft = (margin * loop) + "vw";
   loop++;
