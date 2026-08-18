b15593c40613c54ea30bf403667f294f6eb8d793
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2026-08-18 14:25:26 +0200

    Test more zoom

--- find/babaMap.js
+++ find/babaMap.js
@@ -2,9 +2,9 @@
 var startDistance = 0.00595219899384;
 var map = L.map('map').setView([52.15550, 5.38892], 16 );
 var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    maxZoom: 16.5,
-    minZoom:16.5,
-    zoom: 16.5,
+    maxZoom: 16.9,
+    minZoom:16.9,
+    zoom: 16.9,
     zoomControl: false,
     scrollWheelZoom: false,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
