54c16ae83b49ed19ee37846ef407bf3c53b4a451
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2026-08-18 13:20:52 +0200

    Test vast waarde zoom

--- find/babaMap.js
+++ find/babaMap.js
@@ -2,7 +2,9 @@
 var startDistance = 0.00595219899384;
 var map = L.map('map').setView([52.15550, 5.38892],15);
 var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    maxZoom: 17,
+    maxZoom: 16.294718618810897,
+    minZoom:16,
+    setMinZoom:16,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
 OpenTopoMap.addTo(map);
