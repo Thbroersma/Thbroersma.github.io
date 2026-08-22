5b5148ec8dea4a634a05e066f4baef109fffa77b
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2026-08-22 11:33:38 +0200

    Test change right foot

--- find/js/foodMap.js
+++ find/js/foodMap.js
@@ -80,7 +80,7 @@ var imageUrl = "footstepLToLeft.png";
 var imageUrlRight = "footstepRToLeft.png";
 
 var coordinates = L.latLngBounds([52.144403 , 5.401366], [52.144551, 5.401005]);
-var coordinatesRight = L.latLngBounds([52.144685, 5.401645], [52.144855, 5.401221]);
+var coordinatesRight = L.latLngBounds([52.144885, 5.401645], [52.145055, 5.401221]);
 var imageOverlay2 = L.imageOverlay(imageUrlRight, coordinatesRight, {
   className: "right-foot",
 });
