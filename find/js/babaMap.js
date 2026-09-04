cca448e5a21e37393599b7449b7c080419824884
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2026-09-04 13:18:26 +0200

    Wolkje kaart 1 net iets meer naar links

--- find/js/babaMap.js
+++ find/js/babaMap.js
@@ -119,7 +119,7 @@ function getPosition() {
 var popup = L.popup({
       className: "niffler",
     })
-    .setLatLng([52.140749  ,5.399297])
+    .setLatLng([52.140699   ,5.399152])
     .setContent("Niffler")
     .openOn(map);
 
